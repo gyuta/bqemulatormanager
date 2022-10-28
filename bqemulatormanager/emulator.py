c1fdf2be950a14a87f5740df98e06a426d555aee
gyuta <kuroshiba0408@gmail.com>
2022-10-28 12:01:37 +0900

    change logic of port using chek

--- bqemulatormanager/emulator.py
+++ bqemulatormanager/emulator.py
@@ -12,7 +12,7 @@ class Emulator:
         if launch_emulator:
             log_level = 'debug' if debug_mode else 'info'
             
-            if subprocess.run(['lsof', f'-i:{port}'], capture_output=True).stdout:
+            if is_port_in_use(port):
                 raise PortOccupiedError(f'port {port} is occupied.')
             self.running_flg = True
             self.proc = subprocess.Popen(f"bigquery-emulator --project={project_name} --port={port} --log-level={log_level}", shell=True)
@@ -21,3 +21,8 @@ class Emulator:
     def __del__(self):
         if self.running_flg:
             self.proc.terminate()
+
+def is_port_in_use(port: int) -> bool:
+    import socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        return s.connect_ex(('localhost', port)) == 0
